bcb91471c228a14f71205b5c14219df65ba8409a
Ernie van der Meer <ernie.van.der.meer@ing.com>
2022-10-04 09:46:52 +0200

    Add lots of sanity checks to htmltojson

--- modules/htmltojson.js
+++ modules/htmltojson.js
@@ -1,14 +1,40 @@
+const debug = require('debug')('htmltojson');
 const { parse } = require('himalaya');
 
 function htmlToJson(html)
 {
+  if (html === '') { return []; }
+
+  // Parse valid incoming data
   const data = parse(html);
-  const hdr = data.shift();
+  if (! (data instanceof Array))
+  {
+    throw new Error('Failed to parse sqlite3 HTML!');
+  }
+  debug('parsed data:', JSON.stringify(data, null, 2));
+
+  // Filter for only data rows
+  const rows = data
+    .filter(v => v.type === 'element' && v.tagName === 'tr');
+  debug('rows:', JSON.stringify(rows, null, 2));
+
+  // Extract the header, if any
+  const hdr = rows.shift();
+  if (! hdr)
+  {
+    throw new Error('Unable to find any header rows!');
+  }
+  debug('header:', JSON.stringify(hdr, null, 2));
   const fields = hdr.children
     .filter(v => v.type === 'element' && v.tagName === 'th')
     .map(v => v.children.pop().content);
-  const values = data
-    .filter(v => v.type === 'element' && v.tagName === 'tr')
+  if (fields.length === 0)
+  {
+    throw new Error('Unable to detect HTML header <TH> elements! Did you include -header?');
+  }
+  debug('fields:', JSON.stringify(fields, null, 2));
+
+  const values = rows
     .map(v => v.children
       .filter(w => w.type === 'element' && w.tagName === 'td')
       .map(w => w.children.pop().content)
@@ -18,6 +44,8 @@ function htmlToJson(html)
         return a;
       }, {})
     );
+  debug('values:', JSON.stringify(values, null, 2));
+
   return values;
 }
 
--- modules/htmltojson.test.js
+++ modules/htmltojson.test.js
@@ -4,20 +4,41 @@ const { htmlToJson } = require('./htmltojson');
 
 describe('htmltojson', () =>
 {
+  it('properly handles empty sqlite3 html output', () =>
+  {
+    const html = '';
+    expect(htmlToJson(html)).toEqual([]);
+  });
+  it('properly handles sqlite3 html output without header', () =>
+  {
+    const html = '<TABLE><TR><TD>dashboard</TD></TR></TABLE>';
+    expect(() => htmlToJson(html)).toThrow(/Unable to find any header rows/);
+  });
+  it('properly handles non-sqlite3 html', () =>
+  {
+    const html = `</TR>
+    <TR>
+      <TD>dashboard</TD>
+      <TD>no comment</TD>
+    </TR>`;
+    expect(() => htmlToJson(html)).toThrow(/Unable to detect HTML header/);
+  });
   it('properly parses sqlite3 html output', () =>
   {
     const html = `<TR><TH>package</TH>
-<TH>comment</TH>
-</TR>
-<TR><TD>dashboard</TD>
-<TD>no comment</TD>
+      <TH>comment</TH>
     </TR>
-<TR><TD>dashboard-backend</TD>
-<TD>This is a
+    <TR>
+      <TD>dashboard</TD>
+      <TD>no comment</TD>
+    </TR>
+    <TR>
+      <TD>dashboard-backend</TD>
+      <TD>This is a
 comment over multiple lines
 
 with an empty line</TD>
-</TR>`;
+    </TR>`;
     const json = [
       {
         package: 'dashboard',
